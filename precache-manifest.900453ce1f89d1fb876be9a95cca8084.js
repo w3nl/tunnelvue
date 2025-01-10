7bb1ed95e5228a97799d8567f560609ce8acac4d
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2025-01-10 16:28:10 +0100

    Add files

---
+++ precache-manifest.900453ce1f89d1fb876be9a95cca8084.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5544a5eece85a94c20b",
+    "url": "/css/app.aaf4d205.css"
+  },
+  {
+    "revision": "d5544a5eece85a94c20b",
+    "url": "/js/app.6f4262b2.js"
+  },
+  {
+    "revision": "61475e7f0d768889a424",
+    "url": "/js/chunk-6fa2fd60.c2861c10.js"
+  },
+  {
+    "revision": "101a281f851b1ae0bb08",
+    "url": "/js/chunk-vendors.a768075b.js"
+  },
+  {
+    "revision": "da10be97b143de0b98858cb4f1fefbaf",
+    "url": "/img/corner.da10be97.svg"
+  },
+  {
+    "revision": "5d23767fc10f8bdd60c4b806210250bc",
+    "url": "/img/office_bg.5d23767f.jpg"
+  },
+  {
+    "revision": "dc2012a9ce4e797a7c6bfb54f5626bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2558d4c7d1c20493fbacb13a67c81890",
+    "url": "/index.php"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "60d33d29a306200cba8313a66aaac340",
+    "url": "/img/logo.png"
+  }
+];
